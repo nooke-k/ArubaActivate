1e4c6ad8b612322a2d3ad2562ef7735eebb9d308
nko <nooke@iki.fi>
2021-03-27 14:31:44 +0200

    uus

--- inv_query.py
+++ inv_query.py
@@ -10,18 +10,12 @@ cookies = MozillaCookieJar(filename='Activate-cookie.txt')
 cookies.load(filename='Activate-cookie.txt', ignore_discard=True, ignore_expires=False)
 
 url = "https://activate.arubanetworks.com/api/ext/inventory.json?action=query"
-
 resp = s.post(url=url, cookies=cookies)
-
 ap_inventory = resp.json()
-
-#pprint (ap_inventory)
 
 with open ('ap_inventory.json', 'w') as outfile:
  json.dump(ap_inventory,outfile, indent=4)
 
-
-#pprint(dict.values(ap_inventory))
 #pprint (ap_inventory['devices'])
 #pprint(dict.items (ap_inventory))
 ####pprint(dict.items(ap_inventory['devices']))
---
+++ inv_query_v2.py
@@ -0,0 +1,40 @@
+from pprint import pprint
+import requests
+from http.cookiejar import MozillaCookieJar
+import json
+
+## curl -b Activate-cookie.txt https://activate.arubanetworks.com/api/ext/folder.json?action=query
+
+s = requests.session()
+cookies = MozillaCookieJar(filename='Activate-cookie.txt')
+cookies.load(filename='Activate-cookie.txt', ignore_discard=True, ignore_expires=False)
+
+url = "https://activate.arubanetworks.com/api/ext/inventory.json?action=query"
+resp = s.post(url=url, cookies=cookies)
+# print(resp.status_code)
+# print(resp.json())
+count = 0
+if resp and resp.status_code == 200:
+    #print(resp.status_code)
+    ap_inventory = resp.json()
+    folder = input("folder to look for:")
+else:
+    print("ERROR:", resp.status_code, resp)
+
+for data in ap_inventory['devices']:
+    #count += 1
+    #total_num = len(ap_inventory['devices'])
+    data_mac = data['mac']
+    data_folder = data['additionalData']['folder']
+    #print(data_mac, data_folder)
+    if data['additionalData']['folder'] in folder:
+        count +=1
+        print("mac: ", data_mac, "folder:  ",data_folder)
+print ("total aps", count)
+
+
+
+
+
+
+
