63fa4e74d3d991a517e1e131e320c04f308ae962
nko <nooke@iki.fi>
2021-03-27 14:38:06 +0200

    uus

--- inv_query_v2.py
+++ inv_query_v2.py
@@ -10,7 +10,6 @@ resp = s.post(url=url, cookies=cookies)
 
 count = 0
 if resp and resp.status_code == 200:
-    #print(resp.status_code)
     ap_inventory = resp.json()
     folder = input("folder to look for:")
 else:
@@ -19,15 +18,7 @@ else:
 for data in ap_inventory['devices']:
     data_mac = data['mac']
     data_folder = data['additionalData']['folder']
-    #print(data_mac, data_folder)
     if data['additionalData']['folder'] in folder:
-        count +=1
-        print("mac: ", data_mac, "folder:  ",data_folder)
-print ("total aps", count)
-
-
-
-
-
-
-
+        count += 1
+        print("mac: ", data_mac, "folder:  ", data_folder)
+print("total aps", count)
